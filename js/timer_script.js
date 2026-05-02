99dc85dec5bbab5be8baf4f5e2d93ad5614bfcaf
Виктория <achevik08@mail.ru>
2026-05-02 16:57:32 +0800

    Инициализация класса App с базовой структуры

---
+++ js/timer_script.js
@@ -0,0 +1,30 @@
+class App {
+    timer = {
+        min_tens: document.getElementById('min_tens'),
+        min: document.getElementById('min'),
+        sec_tens: document.getElementById('sec_tens'),
+        sec: document.getElementById('sec')
+    };
+
+    #interval
+
+    submit (event) {
+        event.preventDefault();
+        
+    }
+
+    #clearTimer() {
+
+    }
+
+    #startTimer(time) {
+    
+    }
+
+    #setTimer({ min_tens, min, sec_tens, sec }) {
+
+    }
+
+}
+
+const app = new App ();
